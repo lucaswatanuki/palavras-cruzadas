76f11400d822be66f4832697831e14f918496164
rmzanco <rodrigo.zanco@hotmail.com>
2018-11-27 02:23:06 -0200

    correção - conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HMTL>
 <HEAD><title>Conexao PHP - banco</title></HEAD>
 <body>
-<h2>Banco 1</h2>
+<h2>Banco De Palavras</h2>
 <?php
 
 //------Conexao p/ banco--------
@@ -10,7 +10,13 @@
 //username: root
 //senha: *em branco*
 //host: localhost
+//
 //------------------------------
+
+/* MUDANÇAS NO SCRIPT SQL: 
+linha 6: PRIMARY KEY  (`palavra`,`id_grupo`) (exclusao da virgual final, para criação da tabela)
+linha 10-31: INSERT INTO `palavras` VALUES (adicionado o 's' em 'palavras', para a inserção correta na tabela).
+*/
 
 try{
     $username = "root";
@@ -19,20 +25,14 @@ try{
     $id;
     $conn = new PDO('mysql:host=localhost;dbname=banco_palavras', $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare('SELECT palavra FROM palavras WHERE id_grupo = \'comida\' ');
-    $stmt->execute(array('id_grupo'));
 
-    $result = $stmt->fetchAll();
-
-    //$data = $conn->query('SELECT * FROM palavras WHERE id_grupo = '. $conn->quote($name));
+    $stmt = $conn->query("SELECT * FROM palavras");
     
-    if ( count($result) ) { 
-        foreach($result as $row) {
-          print_r($row);
-        }   
-    } else {
-        echo "Nennhum resultado retornado.";
-      }
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "<p>id_grupo: ".$row["id_grupo"]."</p>";
+        echo "<p>palavra: ".$row["palavra"]."</p>";
+        }        
+
 }
 catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
